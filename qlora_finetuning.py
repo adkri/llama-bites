c82ff35a5f3a7343adfd30cf7f30db3a75d75a8b
Amit Adhikari <amitadkri@gmail.com>
2023-09-07 18:30:08 -0700

    cleanup dataset loading

--- qlora_finetuning.py
+++ qlora_finetuning.py
@@ -1,4 +1,13 @@
+import argparse
+import time
 import torch
+import torch.optim as optim
+from collections import namedtuple
+from datasets import load_dataset
+from peft import get_peft_model, prepare_model_for_kbit_training, LoraConfig, PeftModel
+from pathlib import Path
+from torch.utils.data import Dataset
+from torch.optim.lr_scheduler import StepLR
 from tqdm import tqdm
 from transformers import (
     BitsAndBytesConfig, 
@@ -6,15 +15,8 @@ from transformers import (
     LlamaTokenizer,
     default_data_collator,
 )
-from peft import get_peft_model, prepare_model_for_kbit_training, LoraConfig, PeftModel
-from datasets import load_dataset
-from pathlib import Path
 
-from torch.utils.data import Dataset
-from torch.optim.lr_scheduler import StepLR
-import torch.optim as optim
-import time
-
+DatasetItem = namedtuple("DatasetItem", ["input_field", "output_field"])
 
 # //// SETUP ////
 
@@ -22,6 +24,7 @@ import time
 Steps for running the finetuning script:
 1. Configure the hyperparameters in the CONFIG dictionary
 2. Setup the datasets for training and evaluation
+3. Configure the prompt formatter for the dataset
 """
 
 
@@ -29,8 +32,8 @@ Steps for running the finetuning script:
 
 CONFIG = {
     "model_name": "NousResearch/Llama-2-7b-hf",
-    "output_path": "./training_output/",
-    "merged_model_name": "my-merged-name",
+    "lora_output_path": "./training_output/",
+    "finetuned_model_name": "new-model-name",
     "seed": 111111,
     "gamma": 0.85,
     "epochs": 3,
@@ -43,6 +46,7 @@ CONFIG = {
     "should_save_model": True,
 }
 
+
 LORA_CONFIG = LoraConfig(
     r = 8,
     lora_alpha = 32,
@@ -53,6 +57,7 @@ LORA_CONFIG = LoraConfig(
     inference_mode = False,
 )
 
+
 BNB_CONFIG = BitsAndBytesConfig(
     load_in_4bit=True,
     bnb_4bit_quant_type="nf4",
@@ -61,131 +66,100 @@ BNB_CONFIG = BitsAndBytesConfig(
 )
 
 
-# //// DATALOADER ////
-
-def get_dataset(tokenizer, split="train"):
-    # load the dataset from huggingface 
-    hf_dataset_name = "ChrisHayduk/Llama-2-SQL-Dataset"
-    print("Loading dataset from huggingface: ", hf_dataset_name)
-    data = load_dataset(hf_dataset_name, split=split)
-    return FinetuningDataset(tokenizer, data, split=split)
-
-    # load the dataset from local files
-    if split == "train":
-        train_path = Path.cwd() / "data" / "train.csv"
-        print("Loading train dataset from: ", train_path)
-        return FinetuningDataset(tokenizer, local_file=train_path, split="train")
-    else: # eval
-        eval_path = Path.cwd() / "data" / "eval.csv",
-        print("Loading eval dataset from: ", eval_path)
-        return FinetuningDataset(tokenizer, local_file=eval_path, split="eval")
+def get_training_dataset(tokenizer):
+    return get_tokenized_dataset(tokenizer, split="train", source="huggingface", dataset_name="ChrisHayduk/Llama-2-SQL-Dataset")
+    # return get_dataset(None, split="train", source="local", local_file="data/train.csv")  
 
 
-class FinetuningDataset(Dataset):
-    def __init__(self, tokenizer, dataset, local_file=None, split="train"):
+def get_eval_dataset(tokenizer):
+    return get_tokenized_dataset(tokenizer, split="eval", source="huggingface", dataset_name="ChrisHayduk/Llama-2-SQL-Dataset")
+    # return get_dataset(None, split="eval", source="local", local_file="data/train.csv")  
+
+
+def default_prompt_formatter(item: DatasetItem):
+    return f"Query: {item['input']}\nResult: {item['output']}"
+
+
+# //// DATALOADER UTILS ////
+
+def get_tokenized_dataset(tokenizer, split="train", source="huggingface", dataset_name=None, local_file=None, prompt_formatter=default_prompt_formatter):
+    if source == "huggingface":
+        return TokenizedDatasetHF(tokenizer, dataset_name, split, prompt_formatter)
+    elif source == "local":
+        return TokenizedDataset(tokenizer, load_from_csv, local_file, split, prompt_formatter)
+    else:
+        raise ValueError(f"Invalid source: {source}")
+
+
+def load_from_csv(file_path, split="train"):
+    print(f"Loading dataset from csv file: {file_path}")
+    data =  load_dataset("csv", data_files={split: [file_path]}, delimiter=",")
+    for item in data:
+        yield DatasetItem(item["col1"], item["col2"])
+
+
+def load_from_jsonl(file_path, split="train"):
+    print(f"Loading dataset from jsonl file: {file_path}")
+    data =  load_dataset("json", data_files={split: [file_path]})
+    for item in data:
+        yield DatasetItem(item["input"], item["output"])
+
+
+def tokenize_item(tokenizer, item, prompt_formatter, max_length=1024):
+    prompt = prompt_formatter(item)
+    # print(prompt)
+    features = tokenizer(prompt)
+    # pad the input ids and attention mask, fixes issue with mismatch sizes
+    source_ids = features["input_ids"]
+    padded_input_ids = source_ids + [tokenizer.pad_token_id] * (max_length - len(source_ids))
+    attention_mask = features["attention_mask"] + [0] * (max_length - len(source_ids))
+    return {
+        "input_ids": torch.tensor(padded_input_ids, dtype=torch.long),
+        "attention_mask": torch.tensor(attention_mask, dtype=torch.long),
+        "labels": torch.tensor(padded_input_ids, dtype=torch.long),
+    }
+
+class TokenizedDatasetHF(Dataset):
+    def __init__(self, tokenizer, dataset_name, split, prompt_formatter, max_length=512):
         self.tokenizer = tokenizer
-        self.split = "train" if split == "train" else "eval"
-        self.is_local = local_file is not None
-        self.max_length = 1024
-        if dataset is not None:
-            self.dataset = dataset
-        elif local_file is not None:
-            self.dataset = load_dataset(
-                "csv",
-                data_files={
-                    split: [local_file]
-                },
-                delimiter=",",
-            )
+        self.dataset = load_dataset(dataset_name, split=split).select(range(10))
+        self.prompt_formatter = prompt_formatter
+        self.max_length = max_length
 
     def __len__(self):
-        if self.is_local:
-            return self.dataset[self.split].shape[0]
-        return self.dataset.__len__()
-
-    def get_item_features(self, item):
-        # create prompt if needed
-        prompt = f"Query: {item['input']} \n Result: {item['output']}"
-        # print(prompt)
-        sample = self.tokenizer(prompt)
-        return sample
+        return len(self.dataset)
     
     def __getitem__(self, idx):
-        if self.is_local:
-            item = {
-                "input": self.dataset[self.split].iloc[idx]["input"],
-                "output": self.dataset[self.split].iloc[idx]["output"],
-            }
-        else:
-            item = self.dataset.__getitem__(idx)
+        return tokenize_item(self.tokenizer, self.dataset[idx], self.prompt_formatter, self.max_length)
 
-        features = self.get_item_features(item)
 
-        # pad the input ids and attention mask, fixes issue with mismatch sizes
-        source_ids = features["input_ids"]
-        padded_input_ids = source_ids + [self.tokenizer.pad_token_id] * (self.max_length - len(source_ids))
-        attention_mask = features["attention_mask"] + [0] * (self.max_length - len(source_ids))
+class TokenizedDataset(Dataset):
+    def __init__(self, tokenizer, data_source_func, source_path, split, prompt_formatter, max_length=512):
+        self.tokenizer = tokenizer
+        self.data_gen = data_source_func(source_path, split)
+        self.prompt_formatter = prompt_formatter
+        self.max_length = max_length
+        self.len_gen = data_source_func(source_path, split)
+        self._length = 0
 
-        return {
-            "input_ids": torch.tensor(padded_input_ids, dtype=torch.long),
-            "attention_mask": torch.tensor(attention_mask, dtype=torch.long),
-            "labels": torch.tensor(padded_input_ids, dtype=torch.long),
-        }
+    def __len__(self):
+        if self._length == 0:
+            self._length = sum(1 for _ in self.len_gen) # this is expensive
+        return self._length
+
+    def __getitem__(self, idx):
+        return tokenize_item(self.tokenizer, next(self.data_gen), self.prompt_formatter, self.max_length)
 
 
 # //// MAIN DRIVER ////
 
-def main():
-    torch.cuda.manual_seed(CONFIG["seed"])
-    torch.manual_seed(CONFIG["seed"])
-
-    model = init_model()
+def init_tokenizer():
     tokenizer = LlamaTokenizer.from_pretrained(CONFIG["model_name"])
     tokenizer.add_special_tokens({"pad_token": "<PAD>"})
-    model.resize_token_embeddings(len(tokenizer))
-
-    optimizer = optim.AdamW(
-        model.parameters(),
-        lr=CONFIG["learning_rate"],
-        weight_decay=0.0,
-    )
-    lr_scheduler = StepLR(optimizer, step_size=1, gamma=CONFIG["gamma"])
-
-    # get dataloaders
-    train_dataloader = torch.utils.data.DataLoader(
-        get_dataset(tokenizer, split="train"),
-        batch_size=CONFIG["training_batch_size"],
-        num_workers=CONFIG["num_workers_dataloader"],
-        pin_memory=True,
-        drop_last=True,
-        collate_fn=default_data_collator,
-    )
-    eval_dataloader = None
-    if CONFIG["should_run_validation"]:
-        eval_dataloader = torch.utils.data.DataLoader(
-            get_dataset(tokenizer, split="eval"),
-            batch_size=CONFIG["eval_batch_size"],
-            num_workers=CONFIG["num_workers_dataloader"],
-            pin_memory=True,
-            drop_last=True,
-            collate_fn=default_data_collator,
-        )
-
-    # run the training loop
-    results = train(
-        model,
-        tokenizer,
-        optimizer,
-        lr_scheduler,
-        train_dataloader,
-        eval_dataloader,
-    )
-
-    # print results
-    [print(f"{key}: {value}") for key, value in results.items()]
+    return tokenizer
 
 
-def init_model():
+def init_model(tokenizer):
     model = LlamaForCausalLM.from_pretrained(
         CONFIG["model_name"],
         quantization_config=BNB_CONFIG,
@@ -203,7 +177,29 @@ def init_model():
     model = prepare_model_for_kbit_training(model)
     model = get_peft_model(model, LORA_CONFIG)
     model.print_trainable_parameters()
+    model.resize_token_embeddings(len(tokenizer))
     return model
+
+
+def configure_optimizer(model):
+    optimizer = optim.AdamW(
+        model.parameters(),
+        lr=CONFIG["learning_rate"],
+        weight_decay=0.0,
+    )
+    lr_scheduler = StepLR(optimizer, step_size=1, gamma=CONFIG["gamma"])
+    return optimizer, lr_scheduler
+
+
+def get_dataloader(tokenizer, split, batch_size):
+    return torch.utils.data.DataLoader(
+        get_training_dataset(tokenizer=tokenizer) if split == "train" else get_eval_dataset(tokenizer=tokenizer),
+        batch_size=batch_size,
+        num_workers=CONFIG["num_workers_dataloader"],
+        pin_memory=True,
+        drop_last=True,
+        collate_fn=default_data_collator,
+    )
 
 
 def train(
@@ -223,8 +219,8 @@ def train(
 
     # evaluation
     best_val_loss = float("inf")
-    eval_loss = []
-    eval_perp = []
+    eval_losses = []
+    eval_perps = []
     
     # checkpointing
     ckpt_times = []
@@ -257,7 +253,6 @@ def train(
 
         train_epoch_loss = total_loss / len(train_dataloader)
         train_perplexity = torch.exp(train_epoch_loss)
-
         train_perp.append(train_perplexity)
         train_loss.append(train_epoch_loss)
 
@@ -270,15 +265,15 @@ def train(
 
             ckpt_start_time = time.perf_counter()
             if CONFIG["should_save_model"] and eval_loss < best_val_loss:
-                model.save_pretrained(CONFIG['output_path'])
+                model.save_pretrained(CONFIG['lora_output_path'])
             ckpt_end_time = time.perf_counter() - ckpt_start_time
             ckpt_times.append(ckpt_end_time)
 
             if eval_loss < best_val_loss:
                 best_val_loss = eval_loss
                 print(f"New best validation loss: {best_val_loss} on epoch: {epoch}")
-            eval_loss.append(best_val_loss)
-            eval_perp.append(eval_perp)
+            eval_losses.append(best_val_loss)
+            eval_perps.append(eval_perp)
         
         print(f"Epoch: {epoch+1}, train_perplexity: {train_perplexity:.4f}, train_epoch_loss: {train_epoch_loss:.4f}, epoch_time: {epoch_end_time}s")
 
@@ -290,8 +285,8 @@ def train(
         "avg_train_loss": sum(train_loss) / len(train_loss),
     }
     if CONFIG["should_run_validation"]:
-        results["avg_eval_perp"] = sum(eval_perp) / len(eval_perp)
-        results["avg_eval_loss"] = sum(eval_loss) / len(eval_loss)
+        results["avg_eval_perp"] = sum(eval_perps) / len(eval_perps)
+        results["avg_eval_loss"] = sum(eval_losses) / len(eval_losses)
 
     return results
 
@@ -302,7 +297,7 @@ def evaluate(model, tokenizer, eval_dataloader):
     eval_preds = []
     eval_loss = 0.0
 
-    for step, batch in enumerate(tqdm(eval_dataloader, colour="blue", desc="Evaluating")):
+    for _, batch in enumerate(tqdm(eval_dataloader, colour="blue", desc="Evaluating")):
         for key in batch.keys():
             batch[key] = batch[key].to("cuda:0")
         with torch.no_grad():
@@ -323,7 +318,31 @@ def evaluate(model, tokenizer, eval_dataloader):
     return eval_perp, eval_loss
 
 
-def merge_result_output_to_final_model(tokenizer):
+def main_pipeline():
+    torch.cuda.manual_seed(CONFIG["seed"])
+    torch.manual_seed(CONFIG["seed"])
+
+    tokenizer = init_tokenizer()
+    model = init_model(tokenizer)
+    optimizer, lr_scheduler = configure_optimizer(model)
+
+    train_dataloader = get_dataloader(tokenizer, "train", CONFIG["training_batch_size"])
+    eval_dataloader = get_dataloader(tokenizer, "eval", CONFIG["eval_batch_size"]) if CONFIG["should_run_validation"] else None
+
+    results = train(
+        model,
+        tokenizer,
+        optimizer,
+        lr_scheduler,
+        train_dataloader,
+        eval_dataloader,
+    )
+
+    # print results
+    [print(f"{key}: {value}") for key, value in results.items()]
+
+
+def load_and_merge_lora_to_model():
     base_model = LlamaForCausalLM.from_pretrained(
         CONFIG["model_name"],
         low_cpu_mem_usage=True,
@@ -331,22 +350,22 @@ def merge_result_output_to_final_model(tokenizer):
         torch_dtype=torch.float16,
         device_map={"": 0}
     )
-    model = PeftModel.from_pretrained(base_model, CONFIG["output_path"])
+    model = PeftModel.from_pretrained(base_model, CONFIG["lora_output_path"])
     model = model.merge_and_unload()
-
-    # load the tokenizer
-    tokenizer = LlamaTokenizer.from_pretrained(CONFIG["model_name"])
-    tokenizer.add_special_tokens({"pad_token": "<PAD>"})
+    tokenizer = init_tokenizer()
 
     # save the model and tokenizer to the final model name
-    model.save_pretrained(CONFIG["merged_model_name"])
-    tokenizer.save_pretrained(CONFIG["merged_model_name"])
+    model.save_pretrained(CONFIG["finetuned_model_name"])
+    tokenizer.save_pretrained(CONFIG["finetuned_model_name"])
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Run the finetuning script or merge lora to the final model")
+    parser.add_argument("--mode", type=str, choices=["finetune", "merge"], required=True, help="The mode to run the script in, either 'finetune' or 'merge'")
+    args = parser.parse_args()
 
-    # --- merge the output to the final model ---
-    # remove comment and run this after the training is done in new process to avoid memory issues
-    # merge_result_output_to_final_model(tokenizer)
+    if args.mode == "finetune":
+        main_pipeline()
+    elif args.mode == "merge":
+        load_and_merge_lora_to_model()
 
